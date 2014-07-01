83bdbf99b8f13e0d9582ea153afb28b18e50e340
ROSE CASS <rose.cass12@gmail.com>
2014-06-30 17:14:47 -0700

    Addition on Randomize of reviews.

--- themes/simone-child/functions.php
+++ themes/simone-child/functions.php
@@ -3,21 +3,56 @@
 add_theme_support( 'woocommerce' );
 
 /**
-* Remove Category, Price, excerpt, related products
+* Remove Category, Price, excerpt, related products, and upsell display
 *
 * @since 1.0
 * @author Rose
 */
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20 );
 remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15);
-
-
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
 
 /**
-* Change the content within Data Tabs
+* Randomize Reviews at the bottom of Single Product Pages
+*
+* @since 1.0
+* @author Rose
+*/
+
+class wpclass_random_reviews {
+
+	function __construct(){
+
+		add_filter( 'comments_array', array( $this, 'randomize_reviews' ), 10, 2 );
+
+	} // construct
+
+	/**
+	* Randomize Reviews
+	*
+	* @pram array	$comments	required 	array of reviews(comments)
+	* @pram int 	$post_id 	required 	post id we are getting reviews for
+	*
+	*/
+
+	public function randomize_reviews( $comments, $post_id ) {
+
+		if ( get_post_type ( $post_id ) !== 'product' ) return $comments;
+
+		shuffle( $comments );
+
+		return $comments;
+	} //randomize_reviews
+
+} // wpclass_randomize_reviews
+
+new wpclass_random_reviews();
+
+/**
+* Create data tabs for the Description(short excerpt) and features(long description).
 *
 * @since 1.0
 * @author Rose
@@ -72,36 +107,30 @@ function wpclass_featuresdescriptiontabs( $tabs ) {
 }
 
 
-
-function wpclass_reviewtab( $tabs ) { 
-	 	 
-	 	unset( $tabs['description'] ); 
-
-		unset( $tabs['additional_information'] );  	// Remove the additional information tab
-		 
-		return $tabs;
-}
-
-
-add_action('woocommerce_single_product_summary', 'wpclass_featuresdescriptiontabs', 7);
-add_filter('woocommerce_product_tabs', 'wpclass_reviewtab', 10);
-
 /**
-* Randomize Reviews
+* Create Tab for Reviews
 *
 * @since 1.0
 * @author Rose
 */
+function wpclass_reviewtab( $tabs ) { 
+	 	 
+	 	unset( $tabs['description'] ); 				// Remove the description 
+
+		unset( $tabs['additional_information'] );  	// Remove the additional information tab
+		 
+		return $tabs;
+} 
 
 
-
-
+add_action('woocommerce_single_product_summary', 'wpclass_featuresdescriptiontabs', 7); 
+add_filter('woocommerce_product_tabs', 'wpclass_reviewtab', 10);
 
 
 
 
 /**
-* Move the Star Rating
+* Change placement of the Star Rating
 *
 * @since 1.0
 * @author Rose
